f7e8eba582bfd9e0b7c7d4752772b86bf2cdfdd3
suneduraan <sune.duraan@gmail.com>
2023-10-18 20:36:40 +0200

    Added the database.

---
+++ CallCenter/src/models/login.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CallCenter.Models
+{
+	public class login
+	{
+        private int EmployeeID;
+        private string username;
+        private string password;
+
+        public Login()
+        {
+
+        }
+
+        public int EmployeeID1 { get => EmployeeID; set => EmployeeID = value; }
+        public string Username { get => username; set => username = value; }
+        public string Password { get => password; set => password = value; }
+
+    }
+}
+}
