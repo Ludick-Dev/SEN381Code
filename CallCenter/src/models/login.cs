2209f7dbbe505e766600a28022ace77ef5f9c958
suneduraan <sune.duraan@gmail.com>
2023-10-29 22:59:25 +0200

    Added some stored procedures and crud operations (everything is now mixed up I think)

--- CallCenter/src/models/login.cs
+++ CallCenter/src/models/login.cs
@@ -1,11 +1,13 @@
-﻿namespace CallCenter.Models
+﻿using System.Security.AccessControl;
+
+namespace CallCenter.Models
 {
     public class Login
 	{
         private int EmployeeID;
         private string username;
         private string password;
-
+        private string department;
         public Login()
         {
 
@@ -14,7 +16,7 @@
         public int EmployeeID1 { get => EmployeeID; set => EmployeeID = value; }
         public string Username { get => username; set => username = value; }
         public string Password { get => password; set => password = value; }
-
+        public string Department { get => department; set => department = value; }
     }
 }
 
