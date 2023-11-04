cbfde90ac61e863ac6219f13f3577999df28107c
Albrecht Ohsiek <albrecht.ohsiek@eps.ch>
2023-11-04 20:35:47 +0200

    Employee Controller

---
+++ CallCenter/src/controllers/database/EmployeeController.cs
@@ -0,0 +1,114 @@
+using CallCenter.Models;
+using CallCenter.Models.Responses;
+using CallCenter.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CallCenter.Controllers
+{
+    [ApiController]
+    [Route("/api/employee")]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly EmployeeRepository _employeeRepository;
+
+        public EmployeeController(EmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+        }
+
+        [HttpPost("add")]
+        public async Task<IActionResult> AddEmployee([FromBody] AddEmployeeRequest employee)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequestModelStateResponse.BadRequestModelState(ModelState);
+            }
+
+            Employee newEmployee = new Employee()
+            {
+                employeeId = Guid.NewGuid(),
+                employeeName = employee.employeeName,
+                department = employee.department,
+                emailAddress = employee.emailAddress,
+                phoneNumber = employee.phoneNumber
+            };
+
+            await _employeeRepository.AddEmployee(newEmployee);
+            return Ok();
+        }
+
+        [HttpPost("update")]
+        public async Task<IActionResult> UpdateEmployee([FromBody] UpdateEmployeeRequest employee)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequestModelStateResponse.BadRequestModelState(ModelState);
+            }
+
+            Employee existingEmployee = await _employeeRepository.GetEmployeeById(employee.employeeId);
+
+            Employee newEmployee = new Employee()
+            {
+                employeeId = Guid.NewGuid(),
+                employeeName = employee.employeeName ?? existingEmployee.employeeName,
+                department = employee.department ?? existingEmployee.department,
+                emailAddress = employee.emailAddress ?? existingEmployee.emailAddress,
+                phoneNumber = employee.phoneNumber ?? existingEmployee.phoneNumber,
+            };
+
+            await _employeeRepository.UpdateEmployee(newEmployee);
+            return Ok();
+        }
+
+        [HttpGet("get")]
+        public async Task<IActionResult> GetEmployees()
+        {
+            List<Employee> employees = await _employeeRepository.GetAllEmployees();
+            if (employees == null)
+            {
+                return NotFound();
+            }
+            return Ok();
+        }
+
+        [HttpGet("getby/employeeId/{employeeId}")]
+        public async Task<IActionResult> GetEmployeeByEmployeeId([FromRoute] string employeeId)
+        {
+            if (Guid.TryParse(employeeId, out Guid result))
+            {
+                Employee employee = await _employeeRepository.GetEmployeeById(result);
+                if (employee == null)
+                {
+                    return NotFound();
+                }
+                return Ok();
+            }
+            else
+            {
+                return BadRequest(new ErrorResponse("Invalid ID format"));
+            }
+        }
+
+        [HttpGet("getby/employeeName/{employeeName}")]
+        public async Task<IActionResult> GetEmployeeByClientId([FromRoute] string employeeName)
+        {
+                Employee employee = await _employeeRepository.GetEmployeeByName(employeeName);
+                if (employee != null)
+                {
+                    return NotFound();
+                }
+                return Ok();
+        }
+
+        [HttpGet("getby/phoneNumber/{phoneNumber}")]
+        public async Task<IActionResult> GetEmployeeByStatus([FromRoute] string phoneNumber)
+        {
+                Employee employee = await _employeeRepository.GetEmployeeByPhoneNumber(phoneNumber);
+                if (employee != null)
+                {
+                    return NotFound();
+                }
+                return Ok();
+        }
+    }
+}
